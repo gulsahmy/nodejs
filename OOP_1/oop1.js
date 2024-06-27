5421c01507d9f9ffcf9c2f64ffe9ea8b80992cdb
gulsah <gmutluayyigit@gmail.com>
2024-06-27 15:07:14 +0300

    oop1

---
+++ OOP_1/oop1.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const exampleObject = {
+    propertyName: 'value', //property, attribute, field
+    propertyArray: [],
+    propertyObject: {},
+
+    //obje içindeki fonksiyonlara method denir.
+
+    methodName: function () {
+        return 'this is a method'
+    },
+
+    // Alternatif yazım:
+    methodAlternative () {
+        return 'method-2'
+    },
+}
+
+// nokta notasyonmu ile değerler çağrılabilir
+
+console.log(exampleObject.propertyName)
+console.log(exampleObject.methodName())
+
+/* ----------------------------------------------------- */
+
+
+
+
+
+
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
+
