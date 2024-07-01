9b78c80c7bca27a5da5aa14a1320e5b8270100dd
gulsah <gmutluayyigit@gmail.com>
2024-07-01 14:13:57 +0300

    static

--- oop2/oop2.js
+++ oop2/oop2.js
@@ -1,46 +1,42 @@
-'use strict'
+"use strict"
+/* ------------------------------------------------------------------- *
 
-/* ------------------------------------------------- *
+//! Class Declaration: 
+// Class isimleri PascalCase yapıdadır:
+// class PascalNameCase {...}
 
-//* Class Declaration:
+//! Class Expression:
+const PascalNameCase = class{
 
-//class PascalNameCase { ... }
+    undefinedProperty // sadece tanımlanıp bırakılabilir.(değeri undefined)
+    propertyName = 'value' //attribute, field
 
-//* Class Expression:
-
-const PasCalNameCase = class {
-
-    undefinedProperty // sadece tanımlanıp bırakılabilir. ( değeri undefined )
-    propertyName = 'value' // attribute, field
-
-    // method tanımlanırken başına function yazılmaz.
-    methodName(){
-        return ' This is a method'
+    //method tanımlarken başına function yazılmaz:
+    methodName() {
+        return 'This is a method'
     }
-// aşağıdaki yöntem class içinde geçerli değildir.
-//methodName = function() {}
+    // Aşağıdaki yöntem class içinde kullanılmaz:
+    // methodName = function() {}
 }
 
-//? INTANCE = Bir class tan türetilen objedir.
-//? Intance isimleri de PascalCase yapıdadır.
-const NewObj = new PasCalNameCase()
+//! INSTANCE:
+// INSTANCE = Bir class tan türetilen objedir.
+// Instance isimleri de PascalCase yapıdadır.
+const NewObj = new PascalNameCase()
 console.log(NewObj)
 console.log(NewObj.methodName())
-/* ------------------------------------------------- *
 
+/* ------------------------------------------------------------------- *
+//! Constructor Method:
 
-// Contructor Method:
-
-class Car {
-
+class Car{
     isRunning = false
 
-    constructor (brand, model, year = 2000) {
+    constructor(brand, model, year = 2000){
 
         this.brand = brand
         this.model = model
         this.year = year
-
     }
 
     runEngine() {
@@ -49,14 +45,14 @@ class Car {
         return this.isRunning
     }
 
-    newMethod() {
+    newMethod(){
         this.runEngine()
     }
 }
-
 const Ford = new Car('Ford', 'Mustang', 1967)
-// console.log(Ford)
+// console.log(Ford) 
 // Ford.runEngine()
+// Ford.newMethod()
 
 console.log(Ford.isRunning)
 Ford.runEngine()
@@ -65,43 +61,42 @@ console.log(Ford.isRunning)
 const Mercedes = new Car('Mercedes', 'CLK300', 2020)
 console.log(Mercedes)
 
+// class lar soyut, instance lar somuttur.
 
-// class lar soyut, intance lar somut bir datadır.
+const Renault = new Car('Renault', 'Megane', 2010)
+console.log(Renault)
 
-/* ------------------------------------------------- *
+/* ------------------------------------------------------------------- *
+//! INHERITANCE : 
 
-//* INHERITANCE = MirasAlma. Başka bir classın tüm property/methodlarını devralma (parent-child kurulur.)
+//* Inheritance = MirasAlma. Başka bir class ın tüm property/methodlarını devralma.(parent-child ilişkisi kurulur.)
+
 //* SUPER = Parent Class, THIS = Child (Current) Class
 
 class Vehicle {
-
     vehicleIsActive = false
-    vehicleIsActive2 = false
-    vehicleIsActive3 = false
-    vehicleIsActive4 = false
 
-    constructor(vehicleType) {
+    constructor(vehicleType){
         this.vehicleType = vehicleType
     }
 }
 
-class Car extends Vehicle {
+class Car extends Vehicle{
 
     isRunning = false
 
-    // constructor(brand, model, year = 2000) {
+    // constructor(brand, model, year = 2000){
     //     super('Car')
     //     this.brand = brand
     //     this.model = model
     //     this.year = year
     // }
 
-    constructor(brand, model, year, vehicleType) {
+    constructor(brand, model, year, vehicleType){
         super(vehicleType)
         this.brand = brand
         this.model = model
         this.year = year
-
     }
 
     runEngine() {
@@ -111,10 +106,11 @@ class Car extends Vehicle {
     }
 }
 
-// class Truck extends Vehicle { ... }
+// class Truck extends Vehicle {...}
 
-// const Mercedes = new Car('Mercedes', 'CLK300', 2020)
+// const Mercedes = new Car('Mercedes', 'CLK300', 2020 )
 // console.log(Mercedes)
+
 // const Mercedes = new Car('Mercedes', 'CLK300', 2020, 'Car')
 // console.log(Mercedes)
 
@@ -124,47 +120,46 @@ class Accessory extends Car {
         super(brand, model, year, vehicleType)
         this.accessoryName = accessoryName
     }
-
 }
 
 // const BoshClimate = new Accessory('Bosh Climate', 'Ford', 'Mustang', 1967, 'Car')
-// console.log( BoshClimate)
+// console.log(BoshClimate)
 
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 const Mercedes = new Car('Mercedes', 'CLK300', 2020, 'Car')
-console.log( Object.values(Mercedes))
-const BoshClimate = new Accessory('Bosh Climate', Mercedes.brand, Mercedes.model, Mercedes.year, Mercedes.vehicleType)
-console.log( BoshClimate)
+const BoshClimate = new Accessory('Bosh Climate', ...Object.values(Mercedes))
+console.log(BoshClimate)
 
-/* ------------------------------------------------------- *
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+/* ------------------------------------------------------------------- *
+
 //? Polymorphism: Miras aldığımız sınıfın özellik/methodlarını yeniden yazabilme.
 //? - Override: Üst metodla aynı isim ve yapıda yeni bir metod yazma. (ezme / iptal etme / önceliğini alma)
 //? - Overload: Üst metodla aynı isimde ama farklı yapıda (farklı adet/tip) yeni method oluşturma. (aynı anda ikisi de aktif) (JS desteklemez)
-
 
 class Vehicle {
 
     vehicleIsActive = false
 
-    constructor(vehicleType) {
+    constructor(vehicleType){
         this.vehicleType = vehicleType
     }
 
-    getDetails() {
+    getDetails(){
         console.log('Vehicle Class getDetails()')
         return this
     }
 }
 
-class Car extends Vehicle {
+class Car extends Vehicle{
 
     isRunning = false
 
-    constructor(brand, model, year, vehicleType) {
+    constructor(brand, model, year, vehicleType){
         super(vehicleType)
         this.brand = brand
         this.model = model
         this.year = year
-
     }
 
     runEngine() {
@@ -174,68 +169,67 @@ class Car extends Vehicle {
     }
 
     // Override: Parent classtaki methodu aynı isimle tekrar tanımlama.
-    // Override işlemi parent classtaki methodu ezer (önceliğini alır)
-    getDetails() {
+    // Override işlemi parent classtaki methodu ezer.
+    getDetails(){
         console.log('Car Class getDetails()')
         super.getDetails()
     }
 
     //? Overload: Üstteki methodun aynı isim ama farklı parametre adet/tip ile yeniden tanımlanması.
     //? JS Desteklemez. TypeScript destekler.
-    //? Çalışma prensibi: Çağrıldığı zaman parametreye göre ilgili method çalışır.
-    // getDetails(parametre1, parameter2) {
-    //     return this
-    // }
+    //? Çalışma prensibi : Çağrıldığı zaman parametreye göre ilgili method çalışır.
+    // getDetails(parametre1, parametre2){
+    //   return this
+    //  }
 }
 
 const Ford = new Car('Ford', 'Mustang', 1967, 'Car')
 console.log(Ford)
 console.log(Ford.getDetails())
 
-/* ------------------------------------------------------- *
+/* ------------------------------------------------------------------- *
+
 //? Access Modifiers:
 //? - PUBLIC: Genel erişime açık. (Parent: Yes, Child: Yes, Instance: Yes)
-//? - _PROTECTED: Sadece tanımlı olduğu class ve Inherit edilen child-class erişebilir. (Parent: Yes, Child: Yes, Instance: No) (JS Desteklemez.)
-//? - #PRIVATE: Sadece tanımlı olduğu class içinde erişim var. (Parent: Yes, Child: No, Instance: No)
+//? - PROTECTED: Sadece tanımlı olduğu class ve Inherit edilen child-class erişebilir. (Parent: Yes, Child: Yes, Instance: No) (JS Desteklemez.)
+//? - PRIVATE: Sadece tanımlı olduğu class içinde erişim var. (Parent: Yes, Child: No, Instance: No)
 
 class Vehicle {
 
     publicProp = 'parent-child-instance erişebilir.'
-    _proctectedProp = 'parent-child erişebilir.'
-    #privateProp = 'parent erişebilir.'
+    _protectedProp = 'sadece parent-child erişebilir.'
+    #privateProp = 'sadece parent erişebilir.'
 
     vehicleIsActive = false
 
-    constructor(vehicleType) {
+    constructor(vehicleType){
         this.vehicleType = vehicleType
     }
-
-    getDetails() {
+    getDetails(){
         console.log('Vehicle Class getDetails()')
         return this
     }
 }
 
-class Car extends Vehicle {
+class Car extends Vehicle{
 
     isRunning = false
 
-    constructor(brand, model, year, vehicleType) {
+      constructor(brand, model, year, vehicleType){
         super(vehicleType)
         this.brand = brand
         this.model = model
         this.year = year
-
     }
 
     runEngine() {
         this.isRunning = true
         console.log('Engine started')
-        console.log(this.vehicleIsActive) // public: erişebilir, protected: erişebilir, private: erişemez.
+        console.log(this.vehicleIsActive)
         return this.isRunning
     }
 
-    getDetails() {
+    getDetails(){
         console.log('Car Class getDetails()')
         super.getDetails()
     }
@@ -243,29 +237,28 @@ class Car extends Vehicle {
 
 const Ford = new Car('Ford', 'Mustang', 1967, 'Car')
 // console.log(Ford)
-console.log(Ford.runEngine())
-console.log(Ford.vehicleIsActive) // public: erişebilir, protected: erişemez, private: erişemez.
+// console.log(Ford.runEngine())
+console.log(Ford.vehicleIsActive)
 
-/* ------------------------------------------------------- *
 //* Piyasa Standartı İsimlendirme:
 
 const BUYUK_HARF_ISIMLENDIRME = Bu bir constant değişkendir. Developer olarak bunu silme, değiştirirken dikkatli ol.
 const _alttanTireIleBaslayan = BU bir proctected değişkendir. Developer olarak buna dokunma (erişme bile)
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------------------------ */
 //? GETTER & SETTER METHODS: Görevi veri getirme (getter) ve veri güncelleme (setter) olan metodlardır.
 //? "STATIC" KEYWORD: Class'dan direkt erişim. (Instance erişemez.)
+
 
 class Car {
 
     isRunning = false
     #price = 99
 
-    constructor(brand, model, year) {
+      constructor(brand, model, year){
         this.brand = brand
         this.model = model
         this.year = year
-
     }
 
     runEngine() {
@@ -279,42 +272,41 @@ class Car {
         return this.#price
     }
 
-    set setPrice(price) {
+    set setPrice(price){
         console.log('Fiyat güncellendi.')
-        this.#price = price
+        this.#price = price        
     }
-
-    // Statics:
 
     static staticProp = 'bu bir static değerdir.'
 
-    static staticMethod() {
+    static staticMethod(){
         return 'bu bir static methoddur.'
     }
-
 }
 
 const Ford = new Car('Ford', 'Mustang', 1967)
-// console.log(Ford)
-// // console.log(Ford.price) // Private olduğu için erişilemez.
-// // console.log(Ford.getPrice()) // Normal Method
-// console.log(Ford.getPrice) // Getter methodlar bir property gibi çağrılır. (parantez yok)
-// // Ford.setPrice(80000) // Normal method
-// Ford.setPrice = 80000 // Setter methodlar bir propertyy gibi güncellenebilir.
+console.log(Ford)
+// console.log(Ford.price)
+// console.log(Ford.getPrice)
+// // Ford.setPrice(8000)
+// Ford.setPrice = 8000
 // console.log(Ford.getPrice)
 
-//* STATICS
-// Static değerlere sadece ana CLASS ile erişilebilir.
-console.log( Car.staticProp )
-console.log( Car.staticMethod() )
-console.log( Car )
-// Static özellik/metodlara instance ile erişilemez.
-console.log( Ford.staticProp )
+//* STATIC
+console.log(Car.staticProp)
+console.log(Car.staticMethod())
+console.log(Car)
+
+// static özellik ve method lara intance ile erişilemez.
+// static değerlere sadece ana class ile erişilebilir.
+
+console.log(Ford.staticProp)
 
 
-/* ------------------------------------------------------- */
-//? ABSTRACTION: Soyutlama/Modelleme (Class ile obje üretebilme. Aynı amaç için kullanılan değişken ve methodların bir class içinde yazıyor olması)
-//? ENCAPCULLATION: Kapsülleme/Ayrıştırma (Kodların gizliliği, private değişkenlere erişilemiyor olması ve birbirinden bağımsız çalışmaları.)
-/* ------------------------------------------------------- */
 
 
+
+
+
+/* ------------------------------------------------------------------- */
+/* ------------------------------------------------------------------- */
